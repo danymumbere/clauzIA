edf0949f90d19730686136a540cd406b3ebb0baa
danymumbere <danymumbere6@gmail.com>
2026-09-17 14:26:26 +0200

    Brevo Remplace Resend et utilise Nodemailer

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,13 +1,26 @@
-const { Resend } = require("resend");
-const resend = new Resend(process.env.RESEND_API_KEY);
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  await resend.emails.send({
-    from: "ClauzIA <onboarding@resend.dev>", // Ou ton propre domaine validé
+  const transporter = nodemailer.createTransport({
+    host: "smtp-relay.brevo.com",
+    port: process.env.SMTP_PORT || 587,
+    secure: false, // false pour le port 587 (utilise STARTTLS)
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `"ClauzIA" <${process.env.EMAIL_FROM}>`,
     to: options.email,
     subject: options.subject,
     html: options.message,
-  });
+  };
+
+  // Envoie l'email. Si Brevo refuse l'envoi, une erreur sera automatiquement levée 
+  // et attrapée par le catch de ton fichier authRoutes.js
+  await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
